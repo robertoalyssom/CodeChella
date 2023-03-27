e08b58cb0bb567a194529cc15da325d8e675db7f
robertoalyssom <robert_alys@hotmail.com>
2023-03-27 14:20:41 -0300

    refatoração e customização de links, botões e icones na página informacoes

--- js/informacoes/informacoes.js
+++ js/informacoes/informacoes.js
@@ -6,15 +6,22 @@ function clickEvent(question) {
     question.addEventListener("click", event => {
         event.preventDefault();
         showHideAnswer(question);
+        rotateIcon(question);
     });
 };
 
 function showHideAnswer(question) {
     let answerBlock = question.parentNode.childNodes[3]; // div
 
-    if(answerBlock.style.display == "block") {
+    if (answerBlock.style.display == "block") {
         answerBlock.style.display = "none";
     } else {
         answerBlock.style.display = "block";
     };
+};
+
+function rotateIcon(question) {
+    let $icon = question.firstElementChild;
+
+    $icon.classList.toggle("icon-rotate");
 };
